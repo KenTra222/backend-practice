4cd8eede00c0eb63af5240d145736d7075a72d3b
KenTra222 <Kentraviousc@gmail.com>
2023-08-13 21:00:02 +0000

    installed packages

---
+++ npm-practice/src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+ 
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>This is a practice plate for backend development</h1>
+    </>
+  )
+}
+
+export default App
